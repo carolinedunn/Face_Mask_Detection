2d5681e9d5bc627295ddd7bd17c24dfee162655a
Caroline Dunn <thecarolinedunn@gmail.com>
2020-10-23 15:07:23 -0400

    Update detect_mask_webcam_buzzer.py

--- detect_mask_webcam_buzzer.py
+++ detect_mask_webcam_buzzer.py
@@ -133,19 +133,12 @@ while True:
 			red.off()
 			green.on()
 		else:
-			label = "Please wear your face mask"
+			label = "No Face Mask Detected"
 			color = (0, 0, 255)
 			buzzer.on()
 			green.off()
 			red.on()
 		
-		#label = "Thank you" if mask > withoutMask else "Please wear your face mask"
-		#color = (0, 255, 0) if label == "Thank you" else (0, 0, 255)
-
-		# include the probability in the label
-		#label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-		
-
 		# display the label and bounding box rectangle on the output
 		# frame
 		cv2.putText(frame, label, (startX-50, startY - 10),
@@ -156,16 +149,6 @@ while True:
 	cv2.imshow("Face Mask Detector", frame)
 	key = cv2.waitKey(1) & 0xFF
 	
-	#if no mask detected, buzzer on and red light, otherwise green
-	# if withoutMask > mask:
-		# buzzer.on()
-		# green.off()
-		# red.on()
-	# else:
-		# buzzer.off()
-		# red.off()
-		# green.on()
-
 	# if the `q` key was pressed, break from the loop
 	if key == ord("q"):
 		break
